c10d1127c3ce842f858b9a55b54b8e69094f386d
gambitmis <gambitis@gmail.com>
2021-07-02 16:17:14 +0700

    update add flask env

--- app.py
+++ app.py
@@ -4,9 +4,44 @@ import datetime,pytz
 import speedtest
 import inspect
 
+ver = os.environ['version']
+release = os.environ['releases']
+
+def getHostnameAndIpAddr():
+    try:
+        hostName = socket.gethostname()
+        hostIP = socket.gethostbyname(hostName)
+        return hostName, hostIP
+    except:
+        hostName = "Error"
+        hostIP = "Error"
+        return hostName, hostIP
+
+def getNow():
+    try:
+        tz_th = pytz.timezone('Asia/Bangkok')
+        now_utc = datetime.datetime.now(tz=pytz.UTC)
+        now_th = now_utc.astimezone(tz_th)
+    except:
+        print("Error can not get time for now")
+    return now_th,now_utc
+
+def getFlaskEnv(req):
+    try:
+        flask_env = {
+            "remoteAddr":req['REMOTE_ADDR'],
+            "userAgent":req['HTTP_USER_AGENT']
+        }
+    except:
+        print("Error can not get Flask env")
+    return flask_env
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    hostname,ipaddr = getHostnameAndIpAddr()
+    now_th,now_utc = getNow()
+    flask_env = getFlaskEnv(request.environ)
+    return "<p>Time:\"{}\"**** Hostname:\"{}\"**** CONTAINER_IP:\"{}\"**** REQUEST_IP:\"{}\"**** version:\"{}-{}\" </p>".format(now_th,hostname,ipaddr,flask_env['remoteAddr'],ver,release)
   
